142308dc6320cdc3ab0bb151b5b7d6113d537b35
harrytrivedi <p2778184@my365.dmu.ac.uk>
2025-02-16 01:16:37 +0000

    error fixing v1

--- admin/connection.php
+++ admin/connection.php
@@ -1,21 +1,18 @@
 <?php
+// Read connection details from environment variables
+$hostName   = getenv('DB_HOST');
+$dbUser     = getenv('DB_USER');
+$dbPassword = getenv('DB_PASSWORD');
+$dbName     = getenv('DB_NAME');
+$dbPort     = getenv('DB_PORT') ?: '5432';
 
-// PostgreSQL connection settings
-$hostName   = "dpg-cuoi0m5ds78s738l4rh0-a.oregon-postgres.render.com";
-$dbUser     = "parth_video_user";
-$dbPassword = "IAA2bTsNWd25P4el83B5syQbtohv7QAU";
-$dbName     = "parth_video";
-$dbPort     = "5432";
+// For Render, PostgreSQL often requires SSL. Add sslmode=require.
+$connString = "host=$hostName port=$dbPort dbname=$dbName user=$dbUser password=$dbPassword sslmode=require";
 
-// Build the connection string for PostgreSQL
-$connString = "host=$hostName port=$dbPort dbname=$dbName user=$dbUser password=$dbPassword";
-
-// Connect using pg_connect
+// Connect using pg_connect (for PostgreSQL)
 $conn = pg_connect($connString);
 
 if (!$conn) {
-    die("Something went wrong while connecting to PostgreSQL.");
+    die("Something went wrong while connecting to PostgreSQL: " . pg_last_error());
 }
-
-// Connection successful!
 ?>
